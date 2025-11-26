77f56a41daefbc2ccafa581366b0dc1d4c158a2d
Isuru <isurupeiris999@gmail.com>
2025-11-27 09:47:25 +1300

    Add Reading class for sensor data representation

---
+++ Reading.cs
@@ -0,0 +1,9 @@
+namespace SensorSolutions
+{
+    public class Reading
+    {
+        public string SensorName { get; set; }
+        public string Timestamp { get; set; }
+        public double Value { get; set; }
+    }
+}
